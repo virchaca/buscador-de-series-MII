6bb6d9f759140380dabf64e5b421499a0f69c8b9
virchaca <virginia.alvarez82@gmail.com>
2025-03-01 12:12:28 +0100

    add no-image and clean js

--- js/game.js
+++ js/game.js
@@ -1,5 +1,157 @@
 "use strict";
 
+//constantes
+const select = document.querySelector(".js-select");
+const btnPlay = document.querySelector(".js-btnPlay");
+const result = document.querySelector(".js-result"); 
+const playerResult = document.querySelector(".js-playerResult");
+const computerResult = document.querySelector(".js-computerResult");
+const pPlayer = document.querySelector(".js-playerScore");
+const pComputer = document.querySelector(".js-computerScore");
+const pRound = document.querySelector(".js-roundScore");
+const btnInstructions = document.querySelector(".js-btnInstructions");
+const instructionsList = document.querySelector(".js-instructionsList");
+
+// Variables
+let roundsPlayed = 0; 
+let playerScore = 0; 
+let computerScore = 0; 
+
+//funciones
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+  //ceil redondea hacia arriba
+}
+
+function updateScores() {
+  pPlayer.innerHTML = playerScore; 
+  pComputer.innerHTML = computerScore; 
+  pRound.innerHTML = roundsPlayed; 
+}
+
+function getResult() {
+  const resultUsuaria = select.value;
+  const randonNumber = getRandomNumber(9);
+  console.log(randonNumber);
+
+  if (randonNumber <= 3) {
+    //piedra
+    if (resultUsuaria === "piedra") {
+      result.innerHTML = "Empate";
+      playerResult.innerHTML = "con PIEDRA";
+      computerResult.innerHTML = "con PIEDRA";
+      roundsPlayed++;
+    } else if (resultUsuaria === "papel") {
+      result.innerHTML = "Has ganado";
+      playerResult.innerHTML = "con PAPEL";
+      computerResult.innerHTML = "con PIEDRA";
+      playerScore++; 
+      roundsPlayed++;
+    } else if (resultUsuaria === "tijera") {
+      result.innerHTML = "Has Perdido";
+      playerResult.innerHTML = "con TIJERA";
+      computerResult.innerHTML = "con PIEDRA";
+      computerScore++; 
+      roundsPlayed++;
+    }
+  } else if (randonNumber >= 7) {
+    //papel
+    if (resultUsuaria === "piedra") {
+      result.innerHTML = "Has Perdido";
+      playerResult.innerHTML = "con PIEDRA";
+      computerResult.innerHTML = "con PAPEL";
+      computerScore++; 
+      roundsPlayed++;
+    } else if (resultUsuaria === "papel") {
+      result.innerHTML = "Empate";
+      playerResult.innerHTML = "con PAPEL";
+      computerResult.innerHTML = "con PAPEL";
+      roundsPlayed++;
+    } else if (resultUsuaria === "tijera") {
+      result.innerHTML = "Has ganado";
+      playerResult.innerHTML = "con TIJERA";
+      computerResult.innerHTML = "con PAPEL";
+      playerScore++; 
+      roundsPlayed++;
+    }
+  } else if (randonNumber >= 4 && randonNumber <= 6) {
+    //tijera
+    if (resultUsuaria === "piedra") {
+      result.innerHTML = "Has ganado";
+      playerResult.innerHTML = "con PIEDRA";
+      computerResult.innerHTML = "con TIJERA";
+      playerScore++; 
+      roundsPlayed++;
+    } else if (resultUsuaria === "papel") {
+      result.innerHTML = "Has perdido";
+      playerResult.innerHTML = "con PAPEL";
+      computerResult.innerHTML = "con TIJERA";
+      computerScore++; 
+      roundsPlayed++;
+    } else if (resultUsuaria === "tijera") {
+      result.innerHTML = "Empate, los dos habeis sacado TIJERA";
+      playerResult.innerHTML = " con TIJERA";
+      computerResult.innerHTML = "con TIJERA";
+      roundsPlayed++;
+    }
+  }  
+  updateScores(); 
+}
+
+function handleClick(event) {
+  event.preventDefault();
+  if (roundsPlayed < 3) {
+    getResult();
+  } else {
+    result.textContent =
+      '¡Juego terminado! Haz clic en l botón para empezar de nuevo.';
+  }
+}
+
+function handleClickInstructions(event) {
+  event.preventDefault();
+  instructionsList.classList.toggle("hidden");
+}
+
+//eventos
+btnPlay.addEventListener("click", handleClick);
+
+btnInstructions.addEventListener("click", handleClickInstructions);
+
+/*** a los 3 clicks se acaba la partida -- se resetea 
+ * ***/
+
+function reset() {
+  if (playerScore > computerScore) {
+    result.innerHTML = "Gana player!!";
+  } else if (playerScore < computerScore) {
+    result.innerHTML = "Gana computer!!";
+  } else {
+    result.innerHTML = "Empatados! Vuelve a probar!";
+  }
+}
+
+let end = 0; // num de ronda
+const resetGame = document.querySelector(".js-reset");
+const pEnd = document.querySelector(".js-pEnd");
+
+btnPlay.addEventListener("click", () => {
+  if (end < 3) {
+    end++;
+    if (end === 3) {
+      resetGame.classList.remove("hidden");
+      pEnd.classList.remove("hidden");
+      reset();
+    }
+  }
+});
+resetGame.addEventListener("click", () => {
+  location.reload();
+});
+
+
+
+
 /*
 1- crear html: select, btn, texto vamos a jugar (al arrancar la pagina) y parrafos resultados
 2- dar clases a todo y traerlo a JS --> crear constantes
@@ -24,8 +176,6 @@
     - CONDICIONES = si el numero usuaria === numero computer --> EMPATE
                     si num usuaria
                     si num usuaria
-
-
                     piedra> tijera
                     tijar>papel
                     papel>piedra
@@ -37,158 +187,3 @@ function handleClick(event) {
     
 cuando la usuaria selecciona una opcion y le da a jugar al btn, se genera un nuemro aleatorio y entran los condicionales --> se pinta en parrafo result si GANA, PIERDE, EMPATA
 */
-
-/*
-BONUS: Se acumulan los resultados hasta 10 vueltas
-*/
-
-//constantes
-const select = document.querySelector(".js-select");
-const btnPlay = document.querySelector(".js-btnPlay");
-const result = document.querySelector(".js-result"); 
-const playerResult = document.querySelector(".js-playerResult");
-const computerResult = document.querySelector(".js-computerResult");
-const pPlayer = document.querySelector(".js-playerScore");
-const pComputer = document.querySelector(".js-computerScore");
-const pRound = document.querySelector(".js-roundScore");
-const btnInstructions = document.querySelector(".js-btnInstructions");
-const instructionsList = document.querySelector(".js-instructionsList");
-
-// Variables
-let roundsPlayed = 0; //new
-let playerScore = 0; //new
-let computerScore = 0; //new
-
-//funciones
-
-function getRandomNumber(max) {
-  return Math.ceil(Math.random() * max);
-}
-
-function updateScores() {
-  //new
-  pPlayer.innerHTML = playerScore; //new
-  pComputer.innerHTML = computerScore; //new
-  pRound.innerHTML = roundsPlayed; //new
-}
-
-function getResult() {
-  const resultUsuaria = select.value;
-  const randonNumber = getRandomNumber(9);
-  console.log(randonNumber);
-
-  if (randonNumber <= 3) {
-    //piedra
-    if (resultUsuaria === "piedra") {
-      result.innerHTML = "Empate";
-      playerResult.innerHTML = "PIEDRA";
-      computerResult.innerHTML = "PIEDRA";
-      roundsPlayed++;
-    } else if (resultUsuaria === "papel") {
-      result.innerHTML = "Has ganado";
-      playerResult.innerHTML = "PAPEL";
-      computerResult.innerHTML = "PIEDRA";
-      playerScore++; //new
-      roundsPlayed++;
-    } else if (resultUsuaria === "tijera") {
-      result.innerHTML = "Has Perdido";
-      playerResult.innerHTML = "TIJERA";
-      computerResult.innerHTML = "PIEDRA";
-      computerScore++; //new
-      roundsPlayed++;
-    }
-  } else if (randonNumber >= 7) {
-    //papel
-    if (resultUsuaria === "piedra") {
-      result.innerHTML = "Has Perdido";
-      playerResult.innerHTML = "PIEDRA";
-      computerResult.innerHTML = "PAPEL";
-      computerScore++; 
-      roundsPlayed++;
-    } else if (resultUsuaria === "papel") {
-      result.innerHTML = "Empate";
-      playerResult.innerHTML = "PAPEL";
-      computerResult.innerHTML = "PAPEL";
-      roundsPlayed++;
-    } else if (resultUsuaria === "tijera") {
-      result.innerHTML = "Has ganado";
-      playerResult.innerHTML = "TIJERA";
-      computerResult.innerHTML = "PAPEL";
-      playerScore++; 
-      roundsPlayed++;
-    }
-  } else if (randonNumber >= 4 && randonNumber <= 6) {
-    //tijera
-    if (resultUsuaria === "piedra") {
-      result.innerHTML = "Has ganado";
-      playerResult.innerHTML = "PIEDRA";
-      computerResult.innerHTML = "TIJERA";
-      playerScore++; 
-      roundsPlayed++;
-    } else if (resultUsuaria === "papel") {
-      result.innerHTML = "Has perdido";
-      playerResult.innerHTML = "PAPEL";
-      computerResult.innerHTML = "TIJERA";
-      computerScore++; 
-      roundsPlayed++;
-    } else if (resultUsuaria === "tijera") {
-      result.innerHTML = "Empate, los dos habeis sacado TIJERA";
-      playerResult.innerHTML = " TIJERA";
-      computerResult.innerHTML = "TIJERA";
-      roundsPlayed++;
-    }
-  }  
-  updateScores(); //new
-}
-
-function handleClick(event) {
-  event.preventDefault();
-  if (roundsPlayed < 3) {
-    getResult();
-  } else {
-    result.textContent =
-      '¡Juego terminado! Haz clic en "reset" para jugar de nuevo.';
-  }
-}
-
-function handleClickInstructions(event) {
-  event.preventDefault();
-  instructionsList.classList.toggle("hidden");
-}
-//eventos
-btnPlay.addEventListener("click", handleClick);
-
-btnInstructions.addEventListener("click", handleClickInstructions);
-
-/*** a los 10 clicks se acaba la partida -- se resetea 
- * ***/
-
-function reset() {
-  if (playerScore > computerScore) {
-    result.innerHTML = "Gana player!!";
-  } else if (playerScore < computerScore) {
-    result.innerHTML = "Gana computer!!";
-  } else {
-    result.innerHTML = "Empatados! Vuelve a probar!";
-  }
-}
-
-let end = 0; // Inicializamos end en 0
-const resetGame = document.querySelector(".js-reset");
-const pEnd = document.querySelector(".js-pEnd");
-
-btnPlay.addEventListener("click", () => {
-  if (end < 4) {
-    end++;
-    console.log(`end: ${end}`);
-    if (end === 4) {
-      resetGame.classList.remove("hidden");
-      pEnd.classList.remove("hidden");
-      reset();
-      console.log("¡El juego se detiene!");
-    }
-  }
-});
-resetGame.addEventListener("click", () => {
-  location.reload();
-});
--- js/main.js
+++ js/main.js
@@ -1,36 +1,6 @@
 "use strict";
 
-/*pasos
-1 - hacer HTML
-2 - buscar/filtrar series y pelis de TV
-    1- traer constantes
-    2- evento click sobre boton --> ejecuta handle
-    3- funcion handle:
-        - ev.preventDefault()
-        - recoger valor input ¿funcion o constante GLOBAL?
-        - filtrar lo k hay en API con FETCH("https://api.tvmaze.com/search/shows?q=${name}"):
-            Mirar en JSON el nombre de los datos que necesito!!!
-            
-        - PINTAR titulo y foto:
-            - pintar solo name y image --> BUCLE
-            - dar estilo con clase en CSS
-            - CONDICIONAL: si no tiene img, poner la mia por defecto (https://via.placeholder.com/210x295/ffffff/666666/?text=TV)
-
-3 - Favoritas
-    1 - evento click sobre las peliculas --> crear qsALL y currentTarget
-
-    2- Pintar el listado de fav:
-        - El color de fondo y el de fuente se intercambian, indicando que es una serie favorita.
-        - listado en la parte izquierda de la pantalla, debajo del formulario de búsqueda, con las series favoritas --> CREAR ARRAY VACIO PARA METER LAS FAVS, fuera, GLOBAL y LETs 
-        - Las series favoritas deben seguir apareciendo a la izquierda aunque la usuaria realice otra búsqueda. ¿LOCALSTORAGE para FAV? ¿+=?
-
-4. Almacenamiento local en el localStorage. 
-creo const para almacenar, hago un JSON.stringify para el ARRAY de FAVS, luego tendre que llamarlo para que salga por defecto al LEVANTAR la pagina.
-(al recargar la página el listado de favoritos debe mostrarse)
-*/
-
 //constantes
-
 const input = document.querySelector(".js-input");
 const btnS = document.querySelector(".js-search");
 const container = document.querySelector(".js-container");
@@ -38,42 +8,43 @@ const fav = document.querySelector(".js-fav");
 const btnX = document.querySelector(".js-x");
 const btnR = document.querySelector(".js-reset");
 
-//he puesto aqui fuera recoger el balor del input, para crear una constante url, que sea la que le indico al fetch, para tener un codigo más limpio.
+//recogeré el VALUE de input paraponerlo en la URL del fetch ${input.value}.
 
-let searchList = []; //mi array con el obj show, lista de búsqueda
-let favList = []; //lista favoritos
+let searchList = []; //inicializo array de búsqueda vacío
+let favList = []; //inicializo lista favoritos
 const myList = JSON.parse(localStorage.getItem("myShows"));
-//llamo mi constante con la info del localstorage que he guardado tras crear mi lista de favoritos
+//traigo la info del localstorage que he guardado tras crear mi lista de favoritos
 
 if (myList !== null) {
   favList = myList;
   renderListFav();
 }
-//pongo que cuando levante la pagina, si hay info en el localstorage sobre mi lista de favoritos, la muestre/pinte
+//cuando levante la pagina, si hay series favoritas en el localstorage, las pinte
 
 // funciones
 
 function renderList() {
   container.innerHTML = "";
-  // console.log(searchList);
   let classFav = "";
 
   for (let i = 0; i < searchList.length; i++) {
     let src = searchList[i].show.image;
-    let classFav = ""; //para añadir la clase change con estilo cuando esté en favoritos
+    //para acceder a la imagen, po la forma del json
+    let classFav = ""; 
+    //para añadir la clase change con estilo cuando esté en favoritos
 
     const idShow = favList.findIndex(
       (film) => film.show.id === searchList[i].show.id
     );
-    // console.log(idShow);
+
     if (idShow !== -1) {
       classFav = "change";
     }
-    //si el show NO está en favs, findIndex es -1 -> cuando sea distinto, es que si lo tengo en favs--> le añado clase change
+    //si el show NO está en favs, findIndex es -1 -> cuando sea distinto de -1, es que si lo tengo en favs--> le añado clase change
 
     //hago el condicional para la imagen antes de pintar el html
     if (searchList[i].show.image === null) {
-        src = "../images/no-image.webp";
+        src = "../images/no-image.png";
 
     } else {
       src = searchList[i].show.image.medium;
@@ -91,14 +62,14 @@ function renderList() {
   addFav(); //llamo a mi funcion de añadir a favoritos una vez tengo pintada la lista, porque voy a clickar en ella
 }
 
-// Con operador ternario para src: let src = searchList[i].show.image ? searchList[i].show.image.medium : 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+// Con operador ternario para src: let src = searchList[i].show.image ? searchList[i].show.image.medium : '../image/no-image.png';
 
 function getApiInfo() {
   const valueI = input.value;
   fetch(`//api.tvmaze.com/search/shows?q=${valueI}`)
     .then((response) => response.json())
     .then((dataApi) => {
-      console.log(dataApi);
+    //   console.log(dataApi);
       searchList = dataApi;
       renderList(); /*la llamo dentro de la respuesta del servidor, que es cuando me ha llegado la info*/
     });
@@ -137,15 +108,13 @@ function handleAdd(ev) {
 }
 
 function renderListFav() {
-  // console.log('holaaaa');
   fav.innerHTML = "";
   for (let i = 0; i < favList.length; i++) {
     let src2 = favList[i].show.image;
-    //hago el condicional antes de pintar el html
     //tengo que poner en todo favList[i], no favShow, porque ahi le estaría pasando UN elemento
     if (favList[i].show.image === null) {
-      //si la img no tiene url
-      src2 = "../images/no-image.webp";
+      //si la img no tiene url, pongo no-image.png
+      src2 = "../images/no-image.png";
     } else {
       src2 = favList[i].show.image.medium;
     }
@@ -158,22 +127,20 @@ function renderListFav() {
                 </li>`;
   }
   removeFavs(); //llamo a la funcion de borrar favs con X cuando haya pintado la lista!!
-  localStorage.setItem("myShows", JSON.stringify(favList)); //me actualice la info del LocalStorage sin este elemento
+  localStorage.setItem("myShows", JSON.stringify(favList)); 
+  //me actualice la info del LocalStorage sin este elemento
 }
 
 function addFav() {
   const allShows = document.querySelectorAll(".js-li");
-  // console.log(allShows); hago qsALL para seleccionar todos los <li> que haya en la lista search, y recorro el bucle que se forma para poner un evento click al que clicke --> eso lo defino en la funcion handleAdd mediante currentTarget.
+  // qsALL para seleccionar todos los <li> que haya en la lista search, y recorro el bucle que se forma para poner un evento click al que clicke --> eso lo defino en la funcion handleAdd mediante currentTarget.
   for (const item of allShows) {
     item.addEventListener("click", handleAdd);
   }
 }
 
 /****remove from fav by clicking***/
-
 function handleRemoveAdd(e) {
-  // console.log(e.target);
-  // console.log(e.currentTarget.id);
   //pongo el ID al BOTON, que es a quien quiero localizar(al pintar la lista de favs lo ponia al <li>, que era a quien queria pintar.)
 
   const idFav = parseInt(e.currentTarget.id);
@@ -206,12 +173,14 @@ function handleReset() {
 }
 
 //eventos
-
 btnS.addEventListener("click", handleClick);
 btnR.addEventListener("click", handleReset);
 
-// abrir juego PIEDRA PAPEL TIJERA
-document.getElementById("btnAbrirJuego").addEventListener("click", function () {
+
+// abrir juego PIEDRA PAPEL TIJERA en título
+const btnOpenGame = document.getElementById("btnOpenGame");
+
+btnOpenGame.addEventListener("click", function () {
   window.open(
     "./game.html",
     "JuegoPiedraPapelTijera",
@@ -219,24 +188,18 @@ document.getElementById("btnAbrirJuego").addEventListener("click", function () {
   );
 });
 
-// manejar el popup o modal
-const btnAbrirJuego = document.getElementById("btnAbrirJuego");
-const modalJuego = document.getElementById("modalJuego");
-const btnConfirmarJuego = document.getElementById("btnConfirmarJuego");
-const closeModal = document.querySelector(".close");
 
-// Mostrar el modal al hacer clic en "Jugar ahora"
-btnAbrirJuego.addEventListener("click", function () {
-  modalJuego.classList.remove("hidden");
-});
+// manejar el modal
+const modalGame = document.getElementById("modalGame");
+const btnConfirmGame = document.getElementById("btnConfirmGame");
+const closeModal = document.querySelector(".close-modal-js");
 
 closeModal.addEventListener("click", function () {
-  modalJuego.classList.add("hidden");
+    modalGame.classList.add("hidden");
 });
 
-// Iniciar el juego
-btnConfirmarJuego.addEventListener("click", function () {
-  modalJuego.classList.add("hidden"); 
+btnConfirmGame.addEventListener("click", function () {
+    modalGame.classList.add("hidden"); 
   window.open(
     "./game.html",
     "JuegoPiedraPapelTijera",
@@ -245,7 +208,43 @@ btnConfirmarJuego.addEventListener("click", function () {
 });
 
 function showModalAgain() {
-  modalJuego.classList.remove("hidden");
+    modalGame.classList.remove("hidden");
 }
 
 setInterval(showModalAgain, 60000);
+
+
+
+
+
+
+
+
+
+/*pasos
+1 - buscar/filtrar series y pelis de TV
+    1- traer constantes
+    2- evento click sobre boton --> ejecuta handle
+    3- funcion handle:
+        - ev.preventDefault()
+        - recoger valor input ¿funcion o constante GLOBAL?
+        - filtrar lo k hay en API con FETCH("https://api.tvmaze.com/search/shows?q=${name}"):
+            Mirar en JSON el nombre de los datos que necesito!!!
+        - PINTAR titulo y foto:
+            - pintar solo name y image --> BUCLE
+            - dar estilo con clase en CSS
+            - CONDICIONAL: si no tiene img, poner la mia por defecto no-image.png
+
+2 - Favoritas
+    1 - evento click sobre las peliculas --> crear qsALL y currentTarget
+    2 - Pintar el listado de fav:
+        - Al seleccionar una serie los estilos cambian, indicando que es una serie favorita.
+        - listado en la parte izquierda de la pantalla con las series favoritas 
+            --> CREAR ARRAY VACIO PARA METER LAS FAVS, fuera, GLOBAL y LETs 
+        - Las favoritas seguiran en la lista aunque la usuaria realice otra búsqueda.             
+            ¿LOCALSTORAGE para FAV? ¿+=?
+
+3. Almacenamiento local en el localStorage. 
+        creo const para almacenar, hago un JSON.stringify para el ARRAY de FAVS, luego tendre que llamarlo para que salga por defecto al LEVANTAR la pagina. Paso a JSON.parse
+        (al recargar la página el listado de favoritos, si hay, debe mostrarse)
+*/
